11bb5ed8e9ddb4c2a4433b90c089a9fa4df1330c
alexander-russell <alexander.russell4@pm.me>
2025-09-28 22:36:24 +1000

    Format code

--- src/models.rs
+++ src/models.rs
@@ -5,8 +5,8 @@ use diesel::prelude::*;
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::songs)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+#[allow(dead_code)]
 pub struct Song {
     pub id: i32,
     pub path: String
-
 }
